34dd5fc284f836938b76e7b7363dd8f826a76024
vidyanarut <vidyanarute88@gmail.com>
2023-09-20 03:50:35 -0700

    designed project
    designed project

---
+++ 01_variable.js
@@ -0,0 +1,41 @@
+
+var firstName; // variable declaration
+firstName = "undefined";  // Variable initialization
+console.log(firstName);
+firstName = "Omprasad Naurte"; // Variable update
+console.log(firstName);
+firstName = "Omprasad kakaso Narute"; // Variable update
+console.log(firstName);
+firstName = "First Name : Om"; // Variable update
+console.log(firstName);
+firstName = "Last Name : Narute"; // Variable update
+console.log(firstName);
+firstName = "Age:22 Years"; // Variable update
+console.log(firstName);
+firstName = "Address: Sangola"; // Variable update
+console.log(firstName);
+firstName = "PinCode: 413307"; // Variable update
+console.log(firstName);
+firstName = "Male: Male"; // Variable update
+console.log(firstName);
+firstName = "Married Statis : No"; // Variable update
+console.log(firstName);
+
+
+
+
+
+
+/*
+var lastName = "narute"; // variable declaration and initialization
+console.log(lastName);
+
+
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
+*/
---
+++ basic.js
@@ -0,0 +1,12 @@
+a=10;
+b=20;
+c=a+b;
+c=a-b;
+
+
+console.log("addtion of c"+c);
+console.log("c");
+
+
+var firstname;
+firstname = "ujkuihky";
